be56052e9f218e505816e81c30a790c05acbce85
alxmart <alxmartinez@gmail.com>
2023-07-25 18:47:23 -0300

    Created ChamadasFragment and fragment_chamadas.xml.

--- app/src/main/java/com/luizafmartinez/fragments/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/fragments/MainActivity.kt
@@ -2,10 +2,26 @@ package com.luizafmartinez.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.luizafmartinez.fragments.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val fragmentManager = supportFragmentManager.beginTransaction()
+//        // Diversas alterações em fragments
+//        fragmentManager.add( R.id.fragment_conteudo,ConversasFragment() )
+//        fragmentManager.commit()
+
+        supportFragmentManager
+            .beginTransaction()
+            .add( R.id.fragment_conteudo,ConversasFragment() )
+            .commit()
+
     }
 }
+
+
+
+
--- app/src/main/java/com/luizafmartinez/fragments/fragments/ChamadasFragment.kt
+++ app/src/main/java/com/luizafmartinez/fragments/fragments/ChamadasFragment.kt
@@ -7,33 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.luizafmartinez.fragments.R
 
-class ConversasFragment : Fragment() {
+class ChamadasFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-            val view = inflater.inflate(
-                R.layout.fragment_conversas,
-                container,
-                false
-            )
-            // Processamento da Visualização
-            //view.findViewById<>()
-
-            return view
-       }
-
+        return inflater.inflate(
+            R.layout.fragment_chamadas,
+            container,
+            false
+        )
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
