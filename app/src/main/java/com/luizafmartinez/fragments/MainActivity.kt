96cd9b8c895b484ac29a97fd9282c08e764538e2
alxmart <alxmartinez@gmail.com>
2023-07-26 09:10:03 -0300

    Adding click events and testing fragment remove.

--- app/src/main/java/com/luizafmartinez/fragments/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/fragments/MainActivity.kt
@@ -2,22 +2,45 @@ package com.luizafmartinez.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.luizafmartinez.fragments.fragments.ChamadasFragment
 import com.luizafmartinez.fragments.fragments.ConversasFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnConversas : Button
+    private lateinit var btnChamadas  : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnConversas = findViewById(R.id.btn_conversas)
+        btnChamadas  = findViewById(R.id.btn_chamadas)
 
 //        val fragmentManager = supportFragmentManager.beginTransaction()
 //        // Diversas alterações em fragments
 //        fragmentManager.add( R.id.fragment_conteudo,ConversasFragment() )
 //        fragmentManager.commit()
 
-        supportFragmentManager
+        val conversasFragment = ConversasFragment()
+
+        btnConversas.setOnClickListener {
+            supportFragmentManager
             .beginTransaction()
-            .add( R.id.fragment_conteudo,ConversasFragment() )
+            .replace( R.id.fragment_conteudo, conversasFragment )
             .commit()
+        }
+
+        btnChamadas.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                //.replace( R.id.fragment_conteudo,ChamadasFragment() )
+                .remove(conversasFragment)
+                .commit()
+        }
+
+
 
     }
 }
